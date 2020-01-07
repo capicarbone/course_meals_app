978c1499e54b386ff8c168b5500c6c0ea104c687
Carlos <cpinelly@gmail.com>
2020-01-07 08:27:28 -0500

    Added navigation for Meals Screen. Added Meal model.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/categories_screen.dart';
+import 'package:meals_app/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -21,7 +22,11 @@ class MyApp extends StatelessWidget {
               fontWeight: FontWeight.bold),
             ),
       ),
-      home: CategoriesScreen(),
+      //home: CategoriesScreen(),
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+      },
     );
   }
 }
